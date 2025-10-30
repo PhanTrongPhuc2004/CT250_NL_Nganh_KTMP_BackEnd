f7edc1ea0f3e823a11929cbaa06663eaee5e1f9b
phucthienk48 <thiennguyen741235@gmail.com>
2025-10-30 20:32:23 +0700

    donhang

--- src/routes/DonHang.route.js
+++ src/routes/DonHang.route.js
@@ -5,10 +5,13 @@ const DonHangController = require("../controller/DonHang.controller");
 // Tạo đơn hàng
 router.post("/", DonHangController.taoDonHang);
 
-// Lấy đơn hàng theo username
-router.get("/:username", DonHangController.layDonHangTheoUser);
+// Cập nhật trạng thái
+router.put("/:id", DonHangController.capNhatTrangThai);
 
-// (Tuỳ chọn) Lấy tất cả đơn hàng (Admin)
+// Lấy đơn hàng của user theo tên đăng nhập
+router.get("/:tenDangNhap", DonHangController.layDonHangTheoUser);
+
+// Lấy tất cả đơn hàng (admin)
 router.get("/", DonHangController.layTatCaDonHang);
 
 module.exports = router;
--- src/services/DonHang.service.js
+++ src/services/DonHang.service.js
@@ -1,21 +1,25 @@
 const DonHang = require("../models/DonHang.model");
 
 const DonHangService = {
-  // Thêm đơn hàng mới
+  // Tạo đơn hàng mới
   async taoDonHang(data) {
     const donHang = new DonHang(data);
     return await donHang.save();
   },
 
-  // Lấy tất cả đơn hàng của 1 user
-  async layDonHangTheoUser(username) {
-    return await DonHang.find({ tenDangNhap: username }).sort({ date: -1 });
-
+  // Lấy tất cả đơn hàng của 1 user (theo tenDangNhap)
+  async layDonHangTheoUser(tenDangNhap) {
+    return await DonHang.find({ tenDangNhap }).sort({ date: -1 });
   },
 
-  // Lấy toàn bộ đơn hàng (dành cho admin)
+  // Lấy toàn bộ đơn hàng (admin)
   async layTatCaDonHang() {
     return await DonHang.find().sort({ date: -1 });
+  },
+
+  // Cập nhật trạng thái đơn hàng
+  async capNhatTrangThai(id, status) {
+    return await DonHang.findByIdAndUpdate(id, { status }, { new: true });
   },
 };
 
